55f25289e5ab1774193fbf4c10d4768f0512a72f
Camila <bbatistacamila@gmail.com>
2024-07-22 23:02:53 -0300

    🔥Remove obsolete code

--- flask_tcc/main.py
+++ flask_tcc/main.py
@@ -2,14 +2,6 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# dict_item = {
-#     'key1': 'item 1',
-#     'key2': 'item 2',
-# }
-
-# @app.route('/items/<item_id>')
-# def show_item(item_id):
-#     return {'results': dict_item[item_id]}
 
 users = {}
 purchases = {}
