158f561cfd2ec3ad385af1389e2f0838e67cb293
Camila <bbatistacamila@gmail.com>
2024-07-25 20:06:13 -0300

    :recycle: Refactor purchase endpoints

--- flask_tcc/main.py
+++ flask_tcc/main.py
@@ -1,81 +1,3 @@
-# from flask import Flask, request, jsonify
-
-# app = Flask(__name__)
-
-
-# users = {}
-# purchases = {}
-# payments = {}
-
-# @app.route('/users', methods=['POST'])
-# def register_user():
-#     user_id = len(users) + 1
-#     user_data = request.json
-#     users[user_id] = user_data
-#     return jsonify({"message": "User registered successfully", "user_id": user_id}), 201
-
-
-# @app.route('/users/<int:user_id>', methods=['GET'])
-# def get_user(user_id):
-#     user = users.get(user_id)
-#     if user:
-#         return jsonify(user), 200
-#     else:
-#         return jsonify({"message": "User not found"}), 404
-
-
-# @app.route('/purchases', methods=['POST'])
-# def register_purchase():
-#     user_id = request.json.get('user_id')
-#     if not user_id or user_id not in users:
-#         return jsonify({"message": "Invalid or missing user_id"}), 400
-
-#     purchase_id = len(purchases) + 1
-#     purchase_data = request.json
-#     purchases[purchase_id] = purchase_data
-#     return jsonify({"message": "Purchase registered successfully", "purchase_id": purchase_id}), 201
-
-
-# @app.route('/purchases/<int:purchase_id>', methods=['GET'])
-# def get_purchase(purchase_id):
-#     purchase = purchases.get(purchase_id)
-#     if purchase is not None:
-#         return jsonify(purchase), 200
-#     else:
-#         return jsonify({"message": "Purchase not found"}), 404
-
-
-# @app.route('/payments', methods=['POST'])
-# def register_payment():
-#     purchase_id = request.json.get('purchase_id')
-#     if not purchase_id or purchase_id not in purchases:
-#         return jsonify({"message": "Invalid or missing purchase_id"}), 400
-
-#     payment_id = len(payments) + 1
-#     payment_data = request.json
-#     payments[payment_id] = payment_data
-#     purchases[purchase_id]['paid'] = True  # Marca a compra como paga
-#     return jsonify({"message": "Payment registered successfully", "payment_id": payment_id}), 201
-
-
-# @app.route('/payments/<int:payment_id>', methods=['GET'])
-# def get_payment(payment_id):
-#     payment = payments.get(payment_id)
-#     if payment is not None:
-#         return jsonify(payment), 200
-#     else:
-#         return jsonify({"message": "Payment not found"}), 404
-
-
-# @app.route('/paid_purchases', methods=['GET'])
-# def get_paid_purchases():
-#     paid_purchases_count = sum(1 for purchase in purchases.values() if purchase.get('paid'))
-#     return jsonify({"paid_purchases_count": paid_purchases_count}), 200
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask, request, jsonify, abort
 from pydantic import BaseModel, ValidationError
 from typing import Dict, Any
@@ -95,10 +17,13 @@ class UserResponse(UserBase):
     message: str
 
 
-class Purchase(BaseModel):
+class PurchaseBase(BaseModel):
     user_id: int
     item_name: str
     price: float
+
+
+class Purchase(PurchaseBase):
     paid: bool = False
 
 
@@ -117,9 +42,10 @@ class PaymentResponse(Payment):
     message: str
 
 
-users: Dict[int, Dict[str, Any]] = {}
-purchases: Dict[int, Dict[str, Any]] = {}
-payments: Dict[int, Dict[str, Any]] = {}
+# "Database"
+users: Dict[int, User] = {}
+purchases: Dict[int, Purchase] = {}
+payments: Dict[int, Payment] = {}
 
 
 def get_valid_user(user_id):
@@ -145,7 +71,7 @@ def handle_not_found_error(error):
 
 
 # Users
-@app.route('/users/', methods=['POST'])
+@app.route('/users', methods=['POST'])
 def register_user():
     user_data = request.json
     try:
@@ -172,7 +98,7 @@ def get_user(user_id):
 
 
 # Purchases
-@app.route('/purchases/', methods=['POST'])
+@app.route('/purchases', methods=['POST'])
 def register_purchase():
     purchase_data = request.json
     try:
@@ -184,7 +110,7 @@ def register_purchase():
         abort(400, description='Invalid or missing user_id')
 
     purchase_id = len(purchases) + 1
-    purchases[purchase_id] = purchase.dict()
+    purchases[purchase_id] = Purchase.model_validate(purchase.model_dump())
     purchase_response = PurchaseResponse(
         purchase_id=purchase_id,
         message='Purchase registered successfully',
@@ -204,28 +130,28 @@ def get_purchase(purchase_id):
     
     _ = get_valid_user(x_user_id)
     purchase = purchases.get(purchase_id)
-    if purchase and purchase.get('user_id') == x_user_id:
-        return jsonify(purchase)
+    if purchase and purchase.user_id == x_user_id:
+        return jsonify(purchase.dict())
 
     abort(404, description='Purchase not found')
 
 
-@app.route("/all-purchases/", methods=['GET'])
+@app.route('/all-purchases', methods=['GET'])
 def get_all_purchases():
     x_user_id = request.headers.get('x_user_id', type=int)
     if x_user_id is None:
         abort(400, description='Missing x_user_id header')
     
     _ = get_valid_user(x_user_id)
-    user_purchases = {pid: p for pid, p in purchases.items() if p["user_id"] == x_user_id}
+    user_purchases = {pid: p.dict() for pid, p in purchases.items() if p.user_id == x_user_id}
     if user_purchases:
         return jsonify(user_purchases)
 
     abort(404, description="No purchases found")
-
+    
 
 # Payments
-@app.route('/payments/', methods=['POST'])
+@app.route('/payments', methods=['POST'])
 def register_payment():
     payment_data = request.json
     try:
@@ -241,7 +167,7 @@ def register_payment():
 
     payment_id = len(payments) + 1
     payments[payment_id] = payment.dict()
-    purchases[payment.purchase_id]['paid'] = True
+    purchases[payment.purchase_id].paid = True
     payment_response = PaymentResponse(
         payment_id=payment_id,
         user_id=payment.user_id,
@@ -265,7 +191,7 @@ def get_payment(payment_id):
     abort(404, description='Payment not found')
 
 
-@app.route("/all-payments/", methods=['GET'])
+@app.route("/all-payments", methods=['GET'])
 def get_all_payments():
     x_user_id = request.headers.get('x_user_id', type=int)
     if x_user_id is None:
@@ -280,14 +206,14 @@ def get_all_payments():
 
 
 # Admin
-@app.route("/admin/users/", methods=['GET'])
+@app.route("/admin/users", methods=['GET'])
 def get_all_users():
     return jsonify(users)
 
 
-@app.route('/admin/paid_purchases/', methods=['GET'])
+@app.route('/admin/paid_purchases', methods=['GET'])
 def get_paid_purchases():
-    paid_purchases_count = sum(1 for purchase in purchases.values() if purchase.get('paid'))
+    paid_purchases_count = sum(1 for purchase in purchases.values() if purchase.paid)
     return jsonify({'paid_purchases_count': paid_purchases_count})
 
 
